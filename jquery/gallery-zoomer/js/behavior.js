503e35244f35f26ff181d1b06485585083d9d0a9
J. Renée Beach <splendidnoise@gmail.com>
2012-09-15 12:13:09 -0400

    Intermittent commit.
    Signed-off-by: J. Renée Beach <splendidnoise@gmail.com>

--- jquery/gallery-zoomer/js/behavior.js
+++ jquery/gallery-zoomer/js/behavior.js
@@ -1,16 +1,23 @@
 $(document).ready(function (event) {
-  $('img').hover(
-    function (event) {
-      $(this).css({
-        'outline': '3px dotted red'
-      });
-      console.log('hover in');
-    },
-    function (event) {
-      $(this).css({
-        'outline': 'none'
-      });
-      console.log('hover out');
-    }
-  );
+  // Create a wrapper.
+  var $wrapper = $('<div></div>');
+  // Add the class 'image-cropper'
+  $wrapper
+  .addClass('image-cropper');
+  // Get the images on the page.
+  var $img = $('#content img');
+  // Wrap the images in the div wrapper.
+  $img
+  .wrap($wrapper)
+  .parent()
+  .each(function (index, element) {
+    var $parent = $(element);
+    var parentWidth = $parent.width();
+    var parentHeight = $parent.height();
+    // Get the image.
+    var $myImg = $parent.find('img');
+    var width = $myImg.width();
+    var height = $myImg.height();
+    // Get the excess dimensions.
+  });
 });
