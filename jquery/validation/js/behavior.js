271a3a25bbdee604b8ee9900edd8a65cf093f9ac
J. Renée Beach <splendidnoise@gmail.com>
2012-09-20 21:22:25 -0400

    More forms examples.
    Signed-off-by: J. Renée Beach <splendidnoise@gmail.com>

--- jquery/validation/js/behavior.js
+++ jquery/validation/js/behavior.js
@@ -1,11 +1,34 @@
 $(document).ready(function (event) {
-  $('form').on('submit', function (event) {
+  // Get the form.
+  var $form = $('form');
+  // Attach a submit handler.
+  $form.on('submit', function (event) {
     event.preventDefault();
     var $form = $(this);
-    var hunkyDory = false;
+    var isValid = true;
+    // Do some validation!
+    var firstName = $form.find(':text').filter('[name="first_name"]');
+    if (firstName.val().length == 0) {
+      isValid = false;
+      firstName.css({
+        'background-color': 'pink'
+      })
+      .focus();
+    }
+    var $radioGroup = $('#favorite-food');
+    var $radioItem = $form.find(':radio').filter('[name="pizza_topping"]:checked');
+    if ($radioItem.length == 0) {
+      isValid = false;
+      $radioGroup
+      .css({
+        'outline': '1px dotted red'
+      });
 
-    if (hunkyDory) {
+    }
+
+    if (isValid) {
       this.submit();
     }
   });
+
 });
