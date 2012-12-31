20ff96f6481271e9d08a6991c208e7bd6b8aa394
J. Renée Beach <splendidnoise@gmail.com>
2012-12-31 12:42:05 -0500

    General cleanup
    Signed-off-by: J. Renée Beach <splendidnoise@gmail.com>

--- portfolio-viewer/dev/js/main.js
+++ portfolio-viewer/dev/js/main.js
@@ -1,4 +1,23 @@
-var small = 5;
-var big = "2";
-var horse = 70;
-console.log(small + big * horse);
+jQuery('.item').hover(
+  // Hover in.
+  function (event) {
+    var $item = jQuery(this);
+    var $details = $item.find('dl');
+    $details.animate({
+      'bottom': 0,
+      'opacity': 1
+    }, 1000);
+  },
+  // Hover out.
+  function (event) {
+    var $item = jQuery(this);
+    var $details = $item.find('dl');
+    $details.animate({
+      'bottom': '-100%',
+      'opacity': 0
+    }, 250);
+
+  }
+);
+
+
