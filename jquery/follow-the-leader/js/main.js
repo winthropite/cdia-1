87fd9a857d9a45f8f5250b63018908c339330044
J. Renée Beach <splendidnoise@gmail.com>
2013-01-01 12:45:49 -0500

    Made the drag behavior a tad more complex.
    Signed-off-by: J. Renée Beach <splendidnoise@gmail.com>

--- jquery/follow-the-leader/js/main.js
+++ jquery/follow-the-leader/js/main.js
@@ -1,19 +1,24 @@
 $(document).ready(function (event) {
   var $kitten = $('#the-follow');
+  var width = $kitten.width();
+  var height = $kitten.height();
 
   function mouseMoveHandler (event) {
     var left = event.pageX;
     var top = event.pageY;
     $kitten.css(
       {
-        'left': left + 50,
-        'top': top + 50
+        'left': left - (width / 2),
+        'top': top - (height / 2)
       }
     );
   }
-
-  $(document).on('mousemove', mouseMoveHandler);
-  $(document).on('click', function (event) {
+  // Attach event handlers.
+  $kitten.on('mousedown', function (event) {
+    $(document).on('mousemove', mouseMoveHandler);
+  });
+  // Look for mouse up on the document, not just the image being dragged.
+  $(document).on('mouseup', function (event) {
     $(this).off('mousemove');
   });
 });
