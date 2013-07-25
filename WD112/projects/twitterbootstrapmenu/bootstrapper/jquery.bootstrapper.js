bf58b45f54a07fee249ff9a12e7d41d1dbfc1c66
J. Renée Beach <splendidnoise@gmail.com>
2013-07-24 21:37:39 -0400

    bootstrapper plugin and tooltip plugin.

---
+++ WD112/projects/twitterbootstrapmenu/bootstrapper/jquery.bootstrapper.js
@@ -0,0 +1,40 @@
+jQuery.fn.bootstrapper = function () {
+    // This variable exists at the top-most scope of our plugin function.
+    var $root;
+    // Process menus; mark as processed; add expand button.
+    function process ($menu) {
+      // Use the .bootstrap-menu class for theme styling.
+      $menu.addClass('bootstrapper-processed bootstrapper-menu bootstrapper-clearfix');
+      // Wrap the menu.
+      $menu.wrap('<div class="bootstrapper"></div>');
+      // Assign the value of $root, which is declared above.
+      $root = $menu.parent();
+      // Insert a button into our expando plugin.
+      $root.prepend('<button>Expand</button>');
+    }
+    // Save a reference to the menus.
+    var $menu = this;
+    // Initiate menu processing.
+    process($menu);
+
+    // Attach a click handler.
+    $root.on('click', 'button', function () {
+      var $button = $(this);
+      var $menu = $button.next();
+      var isOpen = $menu.hasClass('open');
+
+      // Toggle the menu open or closed.
+      if (isOpen) {
+        $menu.removeClass('open');
+        $menu.slideUp();
+        $button.text('Expand');
+      }
+      else {
+        $menu.addClass('open');
+        $menu.slideDown();
+        $button.text('Collapse');
+      }
+    });
+
+    return this;
+  };
